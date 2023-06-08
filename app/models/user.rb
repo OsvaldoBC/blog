728df7e7a2f4221fd98ac52269da4475742eaec3
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-06-07 20:32:57 -0500

    Add foreign key to associations in the  model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
     has_many :comments
     has_many :like
-    has_many :Post
+    has_many :Post, foreign_key: :author_id
+
+    def recent_posts
+        Post.order(created_at: :desc).limit(3)
+    end
 end

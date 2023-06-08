05a180cc1f5a736adeaad274373baf5710dc1bd6
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-06-08 17:40:56 -0500

    Add unit test for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id
 
   validates :name, presence: { message: "El nombre del usuario no puede estar en blanco" }
-  validates :PostsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: "PostsCounter must be an integer greater than or equal to zero." }
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: "PostsCounter must be an integer greater than or equal to zero." }
 
 
   def recent_posts

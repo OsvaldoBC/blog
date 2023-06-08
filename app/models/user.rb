751023d5fb149ea7c21338e32e7948ac70de839d
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-06-08 18:40:18 -0500

    Add linter style

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,10 @@ class User < ApplicationRecord
   has_many :likes
   has_many :posts, foreign_key: :author_id
 
-  validates :name, presence: { message: "El nombre del usuario no puede estar en blanco" }
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: "PostsCounter must be an integer greater than or equal to zero." }
-
+  validates :name, presence: { message: 'El nombre del usuario no puede estar en blanco' }
+  validates :posts_counter,
+            numericality: { only_integer: true, greater_than_or_equal_to: 0,
+                            message: 'PostsCounter must be an integer greater than or equal to zero.' }
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)

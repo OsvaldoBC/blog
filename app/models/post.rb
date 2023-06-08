6ec897facbb7c7e0dd9539ec3fd80f8ea37dc686
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-06-07 20:41:10 -0500

    add association in the model like and fix the association in the model user an post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+    belongs_to :user, class_name: 'User', foreign_key: :author_id
     has_many :comments
-    has_many :like
+    has_many :likes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_many :comments
-    has_many :like
-    has_many :Post, foreign_key: :author_id
+    has_many :likes
+    has_many :posts, foreign_key: :author_id
 
     def recent_posts
         Post.order(created_at: :desc).limit(3)

b1ae65cdf21a270f609ddfe32e851cb3c0f1044e
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-06-07 21:21:54 -0500

    Add linter style

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,15 @@
 class Post < ApplicationRecord
-    belongs_to :user, class_name: 'User', foreign_key: :author_id
-    has_many :comments
-    has_many :likes
-    
-    after_create :increment_user_posts_counter
+  belongs_to :user, class_name: 'User', foreign_key: :author_id
+  has_many :comments
+  has_many :likes
 
-    def increment_user_posts_counter
-      user.increment!(:posts_counter)
-    end
-  
-    def recent_comments
-      comments.order(created_at: :desc).limit(5)
-    end
+  after_create :increment_user_posts_counter
 
+  def increment_user_posts_counter
+    user.increment!(:posts_counter)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end

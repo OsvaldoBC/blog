240c0e927ba5e7b98855845f3210432020cb6d5c
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-06-14 19:26:49 -0500

    Add user and comments information to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,12 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
   end
 
   def show
     @post = Post.find(params[:id])
+    @comments = @post.comments
+    @user = User.find(params[:user_id])
   end
 end

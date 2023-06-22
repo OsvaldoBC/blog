101f971ddf4b3ad7670744ba8594196e2b94a289
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-06-22 11:23:16 -0500

    Add changes acording the last review

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,6 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
   has_many :posts, foreign_key: :author_id
-  # Roles = [ :admin , :user]
-
   validates :name, presence: { message: 'El nombre del usuario no puede estar en blanco' }
   validates :posts_counter,
             numericality: { only_integer: true, greater_than_or_equal_to: 0,
